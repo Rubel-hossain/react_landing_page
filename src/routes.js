20355341bf849ceed195387cae9d1e8d93f7432c
Rubel-hossain <rubel2585@gmail.com>
2018-04-25 07:45:52 +0600

    starting react page project

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './Home/home';
+ 
+class Routes extends Component {
+
+    render(){
+
+        return(
+
+             <Switch>
+                 <Route exact path="/" component={Home}/>
+             </Switch>   
+  
+        );
+    }
+}
+
+export default Routes;
